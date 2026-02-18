edb229f22937a734b3fb49c355dd00f84600ed91
KIlnur <kilnur1988@rambler.ru>
2026-02-18 08:18:24 +0500

    Добавлено сохранение данных формы отзыва в стейт

--- src/components/reviews/reviews.tsx
+++ src/components/reviews/reviews.tsx
@@ -9,7 +9,7 @@ type Props = {
 export default function Reviews({ reviews }: Props): JSX.Element {
   return (
     <section className="offer__reviews reviews">
-      <h2 className="reviews__title">Reviews &middot; <span className="reviews__amount">1</span></h2>
+      <h2 className="reviews__title">Reviews &middot; <span className="reviews__amount">{reviews.length}</span></h2>
       <ReviewList reviews={reviews} />
       <ReviewForm />
     </section>
